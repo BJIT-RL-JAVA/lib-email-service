09e07eafc6f4af9e2ef23a2b075e170677b3137d
Mallika-BJIT <mallika.dey@bjitgroup.com>
2024-01-23 10:47:30 +0600

    initialize project

---
+++ src/main/java/com/example/libemailservice/LibEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.libemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibEmailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/libemailservice/LibEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libemailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
