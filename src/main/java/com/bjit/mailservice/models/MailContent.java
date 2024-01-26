d581748b472eda69074673206290e206760bcc9d
Mallika-BJIT <mallika.dey@bjitgroup.com>
2024-01-26 12:05:42 +0600

    added file size validation

--- src/main/java/com/bjit/mailservice/models/MailContent.java
+++ src/main/java/com/bjit/mailservice/models/MailContent.java
@@ -1,5 +1,10 @@
 package com.bjit.mailservice.models;
 
+import com.bjit.mailservice.utils.ValidAttachment;
+import com.bjit.mailservice.utils.ValidAttachmentSize;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.groups.Default;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,11 +22,14 @@ import java.util.ArrayList;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MailContent {
+    @Email
     private String from;
-    private ArrayList<String> to;
-    private ArrayList<String> cc;
-    private ArrayList<String> bcc;
+    private ArrayList<@NotNull String> to;
+    private ArrayList<@NotNull String> cc;
+    private ArrayList<@NotNull String> bcc;
     private String subject;
     private String body;
-    private ArrayList<File> attachments;
+
+    @ValidAttachmentSize(groups = {Default.class, ValidAttachment.class})
+    private ArrayList<@NotNull File> attachments;
 }
