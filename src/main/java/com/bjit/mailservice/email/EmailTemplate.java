7fe49b12b0a1010f252c60218060c9db350334d5
Mallika-BJIT <mallika.dey@bjitgroup.com>
2024-02-12 11:26:55 +0600

    added email template

---
+++ src/main/java/com/bjit/mailservice/email/EmailTemplate.java
@@ -0,0 +1,28 @@
+package com.bjit.mailservice.email;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author Mallika Dey
+ */
+public class EmailTemplate {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailTemplate.class);
+
+    @Cacheable("emailTemplateCache")
+    public String getEmailTemplate() {
+        try {
+            ClassPathResource resource = new ClassPathResource("email_template.html");
+            return StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            LOGGER.error(String.format("%s%s", "NOT_FOUND", ex));
+            throw new NotFoundException(ErrorMessages.NOT_FOUND);
+        }
+    }
+}
