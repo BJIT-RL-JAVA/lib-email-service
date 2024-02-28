7a2e4e111e54ef0c5ab66d30b28f2c99a274afa3
Mallika-BJIT <mallika.dey@bjitgroup.com>
2024-02-28 11:12:11 +0600

    handle null exception in sendgrid mail service

--- src/main/java/com/bjit/mailservice/services/MailContentValidation.java
+++ src/main/java/com/bjit/mailservice/services/MailContentValidation.java
@@ -2,14 +2,28 @@ package com.bjit.mailservice.services;
 
 import com.bjit.mailservice.models.MailContent;
 import jakarta.validation.*;
+import org.springframework.util.ObjectUtils;
 
 import java.util.Set;
 
 /**
+ * Utility class for validating {@link MailContent} objects using Java's Bean Validation API.
+ *
  * @author Mallika Dey
  */
 public class MailContentValidation {
+    /**
+     * Validates the provided {@link MailContent} object.
+     *
+     * @param mailContent The {@link MailContent} object to be validated.
+     * @throws IllegalArgumentException       If {@code mailContent} is {@code null}.
+     * @throws ConstraintViolationException If validation fails, containing information about the violations.
+     */
     public static void validateMailContent(MailContent mailContent) {
+        if (ObjectUtils.isEmpty(mailContent)) {
+            throw new IllegalArgumentException("MailContent must not be null");
+        }
+
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         Validator validator = factory.getValidator();
         Set<ConstraintViolation<MailContent>> violations = validator.validate(mailContent);
